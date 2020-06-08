95bdbcca0fa756fdd6e72aed5bbf32388f2fb1ee
ofeck <ofeckm3@gmail.com>
2020-06-08 18:38:26 +0300

    server models and route setting up

--- server/models/user.js
+++ server/models/user.js
@@ -1,4 +1,8 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+require('dotenv').config();
 
 const userSchema =mongoose.Schema({
     email: {
@@ -38,6 +42,53 @@ const userSchema =mongoose.Schema({
         type: String
     }
 });
+
+userSchema.pre('save',function(next){
+    var user = this;
+
+    if(user.isModified('password')){
+        bcrypt.genSalt(10,function(err,salt){
+            if(err) return next(err);
+    
+            bcrypt.hash(user.password,salt,function(err,hash){
+                if(err) return next(err);
+                user.password = hash;
+                next();
+            });
+        })
+    } else{
+        next();
+    }
+})
+
+userSchema.methods.comparePassword = function(currentPassword, callback){
+    bcrypt.compare(currentPassword, this.password, function(err, isMatch){
+        if(err) return callback(err);
+        callback(null, isMatch);
+    });
+}
+
+userSchema.methods.generateToken = function(callback){
+    var user = this;
+    var token = jwt.sign(user._id.toHexString(), process.env.SECRET_PASSWORD);
+
+    user.token = token;
+    user.save(function(err, user) {
+        if (err) return callback(err);
+        callback(null, user);
+    });
+}
+
+userSchema.statics.findByToken = function(token, callback) {
+    var user = this;
+
+    jwt.verify(token, process.env.SECRET_PASSWORD, function(err, decode){
+        user.findOne({ '_id': decode, 'token': token }, function(err, user) {
+            if(err) return callback(err);
+            callback(null, user);
+        });
+    });
+};
 
 const User = mongoose.model('User', userSchema);
 
