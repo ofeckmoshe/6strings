8a66a008bb027aba27d6f550fccc1fe125225422
ofeck <ofeckm3@gmail.com>
2020-06-28 16:12:32 +0300

    added carosel
    added carosel

--- client/src/components/Product/index.js
+++ client/src/components/Product/index.js
@@ -5,8 +5,8 @@ import { connect } from 'react-redux';
 import { getProductDetails, clearProductDetails } from '../../actions/products_actions';
 import PageTop from '../utils/page_top';
 import ProductInfo from './product_info';
-import ProductImage from './productImage';
 
+import Carousel from 'react-bootstrap/Carousel';
 
 
 class ProductPage extends Component {
@@ -20,8 +20,8 @@ class ProductPage extends Component {
     componentWillUnmount() {
         this.props.dispatch(clearProductDetails());
     }
-    
-    
+
+
 
     render() {
         return (
@@ -34,10 +34,24 @@ class ProductPage extends Component {
                         this.props.products.productDetails ?
                             <div className="product_detail_wrapper">
                                 <div className="left">
-                                    <div style={{width:'32em'}}>
-                                        <ProductImage 
-                                            details={this.props.products.productDetails}
-                                        />
+                                    <div style={{width:'32em', margin: '25px'}}>
+                                        {this.props.products.productDetails && 
+                                        <Carousel indicators={false}>
+                                            {this.props.products.productDetails.images.map((img,i) => {
+                                                console.log(img)
+                                                return (
+                                                    <Carousel.Item key={i}>
+                                                        <img
+                                                            style={{height:'300px',maxHeight:'400px', maxWidth:'100%'}}
+                                                            className="d-block w-100"
+                                                            src={`${img.url}`}
+                                                            alt="First slide"
+                                                        />
+                                                    </Carousel.Item>
+                                                            )
+                                                        })
+                                                        }
+                                        </Carousel>}
                                     </div>
                                 </div>
                                 <div className="right">
